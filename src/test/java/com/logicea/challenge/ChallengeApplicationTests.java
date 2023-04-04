c68ab9a2cbaf9f7db2e02df90861029259ff063e
Anggelo Urso <aurso@Radagast.local>
2023-04-03 21:08:24 -0400

    Add squeleton to challenge
    Adding the first squeleton and scaffolding of the project.
    Starting with:
    
     - Creation of the ping controller
    
     - Adding build.gradle and libraries needs to use
    
     - Adding first configuration of JPA and flyway

---
+++ src/test/java/com/logicea/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logicea.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
