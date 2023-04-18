b3e4373b16e384b9e8312d77bc3bdd20c96b083c
Anggelo Urso <anggelo.urso@gmail.com>
2023-04-18 00:26:02 -0400

    feat: Add first load of users to the system

---
+++ src/main/resources/db/migration/V1.1__initial_load_of_users.sql
@@ -0,0 +1,12 @@
+insert into tbl_user ('01afb815-93da-4316-b4ec-d10ea54c1e9d', 'jsmith@logicea.com', 'b444ac06613fc8d63795be9ad0beaf55011936ac');
+insert into tbl_user ('0964b58f-73c3-4be0-a0e9-03dca365d52e', 'janice@logicea.com', '109f4b3c50d7b0df729d299bc6f8e9ef9066971f');
+insert into tbl_user ('5f683337-ce89-44f6-9786-cc083b3a74e9', 'samuel@logicea.com', '3ebfa301dc59196f18593c45e519287a23297589');
+insert into tbl_user ('04a9165d-8482-4abe-b6b1-6cf9d531770f', 'samantha@logicea.com', '1ff2b3704aede04eecb51e50ca698efd50a1379b');
+insert into tbl_user ('bcae2fb9-fa63-454a-8d45-3029b81c0845', 'andrea@logicea.com', '911ddc3b8f9a13b5499b6bc4638a2b4f3f68bf23');
+insert into tbl_user ('305fd29f-a3be-4ef9-93c6-1619e1096f70', 'peter@logicea.com', 'a66df261120b6c2311c6ef0b1bab4e583afcbcc0');
+insert into tbl_user ('c9afda1b-81f0-47e9-af3d-e4b9dfa44d52', 'ruth@logicea.com', 'ea3243132d653b39025a944e70f3ecdf70ee3994');
+insert into tbl_user ('08b1b14e-ab02-43ba-bef4-2b4c97c8cc0e', 'rvalencia@logicea.com', 'd03f9d34194393019e6d12d7c942827ebd694443');
+insert into tbl_user ('46701084-ef48-4623-89de-13fb6ffdcaac', 'sally@logicea.com', 'ea3243132d653b39025a944e70f3ecdf70ee3994');
+insert into tbl_user ('5c616962-b4bf-4036-8afb-60a3db611cac', 'swick@logicea.com', '168f4029f416ee06565f12e697dfc1534ae69d32');
+
+
