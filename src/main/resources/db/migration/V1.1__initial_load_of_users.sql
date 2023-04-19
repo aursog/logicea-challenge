b87d7b3180071abe2e8c670fee3a23119a5bedf6
Anggelo Urso <anggelo.urso@gmail.com>
2023-04-19 05:11:31 -0400

    Add password

--- src/main/resources/db/migration/V1.1__initial_load_of_users.sql
+++ src/main/resources/db/migration/V1.1__initial_load_of_users.sql
@@ -1,10 +1,12 @@
-insert into tbl_user values (unhex(replace('01afb815-93da-4316-b4ec-d10ea54c1e9d', '-', '')), 'jsmith@logicea.com', '$2a$10$9rIa9MbAaRP02XkK6L9EH.5rFvK/HZ63TjxbyszUm.n2lhhgxeR3C', 'member');
-insert into tbl_user values (unhex(replace('0964b58f-73c3-4be0-a0e9-03dca365d52e', '-', '')), 'janice@logicea.com', '$2a$10$jb3iH6IPSUYogt6IU0a9OO1XEWk83kA720vO0H34CpeipDwiYoLCa', 'member');
-insert into tbl_user values (unhex(replace('5f683337-ce89-44f6-9786-cc083b3a74e9', '-', '')), 'samuel@logicea.com', '$2a$10$kHf240zSvXtRqwr9Fh8ObOLt/JLp5qk/xJEsphXeMriJrNrfqJ1F6', 'member');
-insert into tbl_user values (unhex(replace('04a9165d-8482-4abe-b6b1-6cf9d531770f', '-', '')), 'samantha@logicea.com', '$2a$10$h3ylGiEPGs3ro6IMhlbXiO2/wGskU/OEVSip2v3dVmqMlIXPtL2V.', 'member');
-insert into tbl_user values (unhex(replace('bcae2fb9-fa63-454a-8d45-3029b81c0845', '-', '')), 'andrea@logicea.com', '$2a$10$6A//o62EUp.ZVIaQgAqATOqv838f6XPkGlLFReQHaVSVGpAbn1NQa', 'member');
-insert into tbl_user values (unhex(replace('305fd29f-a3be-4ef9-93c6-1619e1096f70', '-', '')), 'peter@logicea.com', '$2a$10$LmlASvI3LoTsce7CkxnY4OoGevK1E0OFNtUKMg2eKkcXXV3BHLVxu', 'member');
-insert into tbl_user values (unhex(replace('c9afda1b-81f0-47e9-af3d-e4b9dfa44d52', '-', '')), 'ruth@logicea.com', '$2a$10$8c74V3WTlwv1K1vm8oBnuefYEOfH/zshZKALp5l43M4L3KOqw8Bn2', 'member');
-insert into tbl_user values (unhex(replace('08b1b14e-ab02-43ba-bef4-2b4c97c8cc0e', '-', '')), 'rvalencia@logicea.com', '$2a$10$S9ajiK/bdnneA43RURyi2.PRsbot8QtTbBSw127469oH25DGUowZq', 'member');
-insert into tbl_user values (unhex(replace('46701084-ef48-4623-89de-13fb6ffdcaac', '-', '')), 'sally@logicea.com', '$2a$10$6Vsa3KLHPhg711C1ybG3RuSjTel.bc8ctKO7iKJ5dngtiF23jDly.', 'member');
-insert into tbl_user values (unhex(replace('5c616962-b4bf-4036-8afb-60a3db611cac', '-', '')), 'swick@logicea.com', '$2a$10$0Qk5ohuaRYxAvCGH5LMMEuMgMvAUWvKFl6muKyU9N4d4w8pHFra66', 'admin');
+insert into tbl_user values (unhex(replace('01afb815-93da-4316-b4ec-d10ea54c1e9d', '-', '')), 'jsmith@logicea.com', '$2a$10$9rIa9MbAaRP02XkK6L9EH.5rFvK/HZ63TjxbyszUm.n2lhhgxeR3C', 'member'); /* test1 */
+insert into tbl_user values (unhex(replace('0964b58f-73c3-4be0-a0e9-03dca365d52e', '-', '')), 'janice@logicea.com', '$2a$10$jb3iH6IPSUYogt6IU0a9OO1XEWk83kA720vO0H34CpeipDwiYoLCa', 'member'); /* test2 */
+insert into tbl_user values (unhex(replace('5f683337-ce89-44f6-9786-cc083b3a74e9', '-', '')), 'samuel@logicea.com', '$2a$10$kHf240zSvXtRqwr9Fh8ObOLt/JLp5qk/xJEsphXeMriJrNrfqJ1F6', 'member'); /* test3 */
+insert into tbl_user values (unhex(replace('04a9165d-8482-4abe-b6b1-6cf9d531770f', '-', '')), 'samantha@logicea.com', '$2a$10$h3ylGiEPGs3ro6IMhlbXiO2/wGskU/OEVSip2v3dVmqMlIXPtL2V.', 'member'); /* test4 */
+insert into tbl_user values (unhex(replace('bcae2fb9-fa63-454a-8d45-3029b81c0845', '-', '')), 'andrea@logicea.com', '$2a$10$6A//o62EUp.ZVIaQgAqATOqv838f6XPkGlLFReQHaVSVGpAbn1NQa', 'member'); /* test5 */
+insert into tbl_user values (unhex(replace('305fd29f-a3be-4ef9-93c6-1619e1096f70', '-', '')), 'peter@logicea.com', '$2a$10$LmlASvI3LoTsce7CkxnY4OoGevK1E0OFNtUKMg2eKkcXXV3BHLVxu', 'member'); /* test6 */
+insert into tbl_user values (unhex(replace('c9afda1b-81f0-47e9-af3d-e4b9dfa44d52', '-', '')), 'ruth@logicea.com', '$2a$10$8c74V3WTlwv1K1vm8oBnuefYEOfH/zshZKALp5l43M4L3KOqw8Bn2', 'member'); /* test7 */
+insert into tbl_user values (unhex(replace('08b1b14e-ab02-43ba-bef4-2b4c97c8cc0e', '-', '')), 'rvalencia@logicea.com', '$2a$10$S9ajiK/bdnneA43RURyi2.PRsbot8QtTbBSw127469oH25DGUowZq', 'member'); /* test8 */
+insert into tbl_user values (unhex(replace('46701084-ef48-4623-89de-13fb6ffdcaac', '-', '')), 'sally@logicea.com', '$2a$10$6Vsa3KLHPhg711C1ybG3RuSjTel.bc8ctKO7iKJ5dngtiF23jDly.', 'member'); /* test9 */
+insert into tbl_user values (unhex(replace('5c616962-b4bf-4036-8afb-60a3db611cac', '-', '')), 'swick@logicea.com', '$2a$10$0Qk5ohuaRYxAvCGH5LMMEuMgMvAUWvKFl6muKyU9N4d4w8pHFra66', 'admin'); /* test10 */
+
+
